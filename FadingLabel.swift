7f40291b3dc2251d31d3d354b602dd56b1b7890a
Shawn Frank <shawn@mindhyve.com>
2022-01-25 08:48:56 +0400

    remove ellipsis from truncated text

--- FadingLabel.swift
+++ FadingLabel.swift
@@ -20,6 +20,13 @@ class FadingLabel: UILabel
         }
     }
     
+    // Used to hold the original un-truncated text
+    private var originalText: String? = ""
+    
+    // Checks if the text is being processed to prevent
+    // the fade animation
+    private var isTruncatingEllipsis = false
+    
     // Add a property observer for numberOfLines changes
     // as only 1 line labels are supported for now
     override var numberOfLines: Int
@@ -46,24 +53,62 @@ class FadingLabel: UILabel
     
     /// The function that handles fading the tail end of the text if the text goes
     /// beyond the bounds of the label's width
-    func fadeTailIfRequired()
+    private func fadeTailIfRequired()
     {
         // Reset the numberOfLines to 1
         numberOfLines = 1
         
-        // Check if the label's has it's width set and if the text goes beyond it's width
-        if bounds.width > CGFloat.zero && intrinsicContentSize.width > bounds.width
+        // Prevent processing fading when the library is in the middle of
+        // processing the string to truncate the ellipsis
+        if !isTruncatingEllipsis
         {
-            // Initialize and configure a gradient to start at the end of
-            // the label
-            let gradient = CAGradientLayer()
-            gradient.frame = bounds
-            gradient.colors = [UIColor.white.cgColor, UIColor.clear.cgColor]
-            gradient.startPoint = CGPoint(x: 0.75, y: 0.5)
-            gradient.endPoint = CGPoint(x: 0.99, y: 0.5)
+            // Check if the label's has it's width set and if the text goes
+            // beyond it's width plus a margin of safety
+            if bounds.width > CGFloat.zero && intrinsicContentSize.width > bounds.width + 5
+            {
+                // Fade label works better with this setting
+                allowsDefaultTighteningForTruncation = true
+                
+                // Initialize and configure a gradient to start at the end of
+                // the label
+                let gradient = CAGradientLayer()
+                gradient.frame = bounds
+                gradient.colors = [UIColor.white.cgColor, UIColor.clear.cgColor]
+                gradient.startPoint = CGPoint(x: 0.8, y: 0.5)
+                gradient.endPoint = CGPoint(x: 0.99, y: 0.5)
+                
+                // Apply the gradient as a mask to the UILabel
+                layer.mask = gradient
+                
+                // Remove ellipsis added as the default UILabel truncation character
+                removeEllipsis()
+                
+                // We do not need to go beyond this point
+                return
+            }
             
-            // Apply the gradient as a mask to the UILabel
-            layer.mask = gradient
+            // If the text has not been truncated, remove the gradient mask
+            if originalText == text
+            {
+                // Remove the layer mask
+                layer.mask = nil
+            }
         }
+    }
+    
+    /// Keep removing 1 character from the label till it no longer needs to truncate
+    private func removeEllipsis()
+    {
+        isTruncatingEllipsis = true
+        
+        // Keep looping till we do not have the perfect string length
+        // to fit into the label
+        while intrinsicContentSize.width > bounds.width
+        {
+            // Drop the last character
+            text = String(text!.dropLast())
+        }
+        
+        isTruncatingEllipsis = false
     }
 }
